995bc740b706284c2b7f4b99ace31e017d72f0a7
Victor Gonzales <vgonzales5157@gmail.com>
2023-04-15 21:52:52 +0800

    portfolio

---
+++ tailwind.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+	theme: {
+		colors: {
+			brand: {
+				DEFAULT: '#fef8ec',
+				icon: '#f5e0b8',
+                box: '#fbf0da',
+				button: '#de6464',
+                buttonHover: '#bb5454',
+				text: '#111111',
+			},
+		}
+	},
+	darkMode: 'class',
+	variants: [
+		'responsive',
+		'group-hover',
+		'focus-within',
+		'first',
+		'last',
+		'odd',
+		'even',
+		'hover',
+		'focus',
+		'active',
+		'visited',
+		'disabled',
+	]
+}
